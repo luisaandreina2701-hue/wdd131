c05eca2ce6332d899396737c84d79119ae205cf1
Luisa <luisaandreina2701@gmail.com>
2026-06-11 23:06:31 -0600

    dice game fix

--- dice-game-example-main/game.js
+++ dice-game-example-main/game.js
@@ -1,32 +1,26 @@
 document.getElementById("rollButton").addEventListener("click", event => {
 
-    //get images
-    const images = document.querySelectorAll("#gameboard img")
+    const images = document.querySelectorAll("#gameboard img");
 
     images.forEach(image => {
-        if(isDieUnlock(image)){
-            image.src = "assets/die_rolling.gif"
+        if (isDieUnlock(image)) {
+            image.src = "assets/die_rolling.gif";
         }
     });
 
     setTimeout(() => {
         images.forEach(image => {
-             if(isDieUnlock(image)){
-            image.src = "assets/die_rolling.gif"
-        }
-            image.src = "assets/white_dice_" + (Math.floor (Math.random() * 6) + 1) + ".gif";
+            if (isDieUnlock(image)) {
+                image.src = "assets/white_dice_" + (Math.floor(Math.random() * 6) + 1) + ".gif";
+            }
         });
-
     }, 2000);
 
 });
 
-
 function isDieUnlock(dieImage) {
     const checkboxes = document.querySelectorAll("#gameboard input");
 
-    const unchecked =  Array.from(checkboxes)
-                            .filter(checkbox => !checkbox.checked);
-
-    return unchecked.find(unchecked => unchecked.className === dieImage.className)
+    return Array.from(checkboxes)
+        .some(checkbox => !checkbox.checked && checkbox.className === dieImage.className);
 }
